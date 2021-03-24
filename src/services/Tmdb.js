6e7433500477928f4b3ee673e26227525f52d0ee
weescoelho <wes.coelho18@gmail.com>
2021-03-23 22:49:07 -0300

    correção no menu mobile

--- src/services/Tmdb.js
+++ src/services/Tmdb.js
@@ -36,11 +36,6 @@ export default {
         title: "Romance",
         items: await basicFetch(`/discover/movie?with_genres=10749&language=pt-BR&api_key=${API_KEY}`),
       },
-      {
-        slug: "documentary",
-        title: "Documentários",
-        items: await basicFetch(`/discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}`),
-      },
     ]
   },
   getMovieExclusive: async () => {
